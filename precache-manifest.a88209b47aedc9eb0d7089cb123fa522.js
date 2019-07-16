fed8756b1923198a29ee6301ce6914dc0c07deb8
Ardani <sulistyoardani@gmail.com>
2019-07-16 21:20:37 +0700

    Updates

---
+++ precache-manifest.a88209b47aedc9eb0d7089cb123fa522.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3569c1c97230170c3791e974de5d3f1",
+    "url": "/porto/index.html"
+  },
+  {
+    "revision": "9266cac84acd9c053771",
+    "url": "/porto/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "52dbad3491e2dd0ef92a",
+    "url": "/porto/static/css/main.246ce3d3.chunk.css"
+  },
+  {
+    "revision": "9266cac84acd9c053771",
+    "url": "/porto/static/js/2.c89ab330.chunk.js"
+  },
+  {
+    "revision": "52dbad3491e2dd0ef92a",
+    "url": "/porto/static/js/main.3d2ba3a5.chunk.js"
+  },
+  {
+    "revision": "3cbf1545047fd652b947",
+    "url": "/porto/static/js/runtime~main.6b01f4fb.js"
+  }
+]);
