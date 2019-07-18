fed2bb604de91662975c010339e00bc227d31d97
Ardani <sulistyoardani@gmail.com>
2019-07-18 10:49:14 +0700

    Updates

--- precache-manifest.e45761f1fcca6ac7f8dbee0e07df3cf5.js
+++ precache-manifest.e45761f1fcca6ac7f8dbee0e07df3cf5.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d3b99db1a11e8242e72c56d767bc5448",
+    "revision": "5f69229db9e39b1f957122efbd1fd9f7",
     "url": "/porto/index.html"
   },
   {
@@ -8,16 +8,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/porto/static/css/2.266e55a5.chunk.css"
   },
   {
-    "revision": "313b131484c6b54700bf",
-    "url": "/porto/static/css/main.0a1a40b7.chunk.css"
+    "revision": "ea883746a69c20eb942c",
+    "url": "/porto/static/css/main.27e20c8d.chunk.css"
   },
   {
     "revision": "a81b53a823a3f3182b33",
     "url": "/porto/static/js/2.d78fa390.chunk.js"
   },
   {
-    "revision": "313b131484c6b54700bf",
-    "url": "/porto/static/js/main.36d24b85.chunk.js"
+    "revision": "ea883746a69c20eb942c",
+    "url": "/porto/static/js/main.65426ad5.chunk.js"
   },
   {
     "revision": "3cbf1545047fd652b947",
